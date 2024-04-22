08eff1c04ceae8b038f5e445578bae4820fe6d84
jlandsmann <git@jlandsmann.de>
2024-04-22 10:19:01 +0200

    add uniform distribution

--- src/main/java/de/jlandsmannn/DecPOMDPSolver/domain/models/utility/Distribution.java
+++ src/main/java/de/jlandsmannn/DecPOMDPSolver/domain/models/utility/Distribution.java
@@ -2,17 +2,34 @@ package de.jlandsmannn.DecPOMDPSolver.domain.models.utility;
 
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Distribution<T> {
     private final Map<T, Double> distribution;
     private final T currentMax;
 
+    public static <T> Distribution<T> createUniformDistribution(Set<T> entries) throws DistributionEmptyException {
+        try {
+            var distribution = entries.stream()
+                    .map(e -> Map.entry(e, 1D / entries.size()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            return new Distribution<T>(distribution);
+        } catch (DistributionSumNotOneException e) {
+            throw new IllegalStateException("Sum of distributions not one", e);
+        }
+    }
+
+    /**
+     * This constructor can be used to create a distribution with probabilities based on distribution
+     * @param distribution A non-empty map of distributions, where the values must sum up to 1
+     * @throws DistributionEmptyException is thrown if distribution is empty
+     * @throws DistributionSumNotOneException is thrown if sum of values in distribution is not one
+     */
     public Distribution(Map<T, Double> distribution) throws DistributionEmptyException, DistributionSumNotOneException {
+        validateDistribution(distribution);
         this.distribution = distribution;
         this.currentMax = calculateMax();
-
-        var sumOfDistributions = this.distribution.values().stream().reduce(Double::sum).orElse(0D);
-        if (sumOfDistributions != 1) throw new DistributionSumNotOneException(sumOfDistributions);
     }
 
     public int size() {
@@ -49,9 +66,20 @@ public class Distribution<T> {
         return distribution.toString();
     }
 
-    private T calculateMax() throws DistributionEmptyException {
+
+    private void validateDistribution(Map<T, Double> distribution) throws DistributionEmptyException, DistributionSumNotOneException {
+        if (distribution.isEmpty()) {
+            throw new DistributionEmptyException();
+        }
+        var sumOfDistributions = distribution.values().stream().reduce(Double::sum).orElse(0D);
+        if (sumOfDistributions != 1) {
+            throw new DistributionSumNotOneException(sumOfDistributions);
+        }
+    }
+
+    private T calculateMax() {
         var entryStream = distribution.entrySet().stream();
-        var maxEntry = entryStream.max(Comparator.comparingDouble(Map.Entry::getValue));
-        return maxEntry.orElseThrow(DistributionEmptyException::new).getKey();
+        var maxEntry = entryStream.max(Comparator.comparingDouble(Map.Entry::getValue)).orElseThrow(IllegalStateException::new);
+        return maxEntry.getKey();
     }
 }
