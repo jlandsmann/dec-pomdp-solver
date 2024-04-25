2374bfb02d5af6fd50c7fe43a5f323b034fa4e39
jlandsmann <git@jlandsmann.de>
2024-04-25 11:06:54 +0200

    add jblas for tests

--- src/main/java/de/jlandsmannn/DecPOMDPSolver/equationSystems/SolverJBLAS.java
+++ src/main/java/de/jlandsmannn/DecPOMDPSolver/equationSystems/SolverJBLAS.java
@@ -1,27 +1,29 @@
 package de.jlandsmannn.DecPOMDPSolver.equationSystems;
 
 import de.jlandsmannn.DecPOMDPSolver.equationSystems.exceptions.SolvingFailedException;
+import org.jblas.DoubleMatrix;
+import org.jblas.Solve;
 import org.ojalgo.RecoverableCondition;
 import org.ojalgo.matrix.decomposition.LU;
 import org.ojalgo.matrix.store.MatrixStore;
 import org.ojalgo.matrix.store.Primitive64Store;
 import org.ojalgo.random.Uniform;
 
-public class SolverOJA {
-    private MatrixStore<Double> a;
-    private MatrixStore<Double> b;
+public class SolverJBLAS {
+    private DoubleMatrix a;
+    private DoubleMatrix b;
     private int numberOfEquations = 0;
     private int numberOfVariables = 0;
 
     public static void main(String[] args) {
-        benchmark(14_000);
+        test();
     }
 
     public static void benchmark(int numberOfVariables) {
-        SolverOJA solver = new SolverOJA()
+        SolverJBLAS solver = new SolverJBLAS()
                 .setDimensions(numberOfVariables, numberOfVariables)
-                .setA(Primitive64Store.FACTORY.makeFilled(numberOfVariables, numberOfVariables, Uniform.of(0, 50)))
-                .setB(Primitive64Store.FACTORY.makeFilled(numberOfVariables, 1, Uniform.of(0, 10)));
+                .setA(DoubleMatrix.rand(numberOfVariables, numberOfVariables))
+                .setB(DoubleMatrix.rand(numberOfVariables, 1));
         var start = System.currentTimeMillis();
         var success = true;
         try {
@@ -35,17 +37,16 @@ public class SolverOJA {
     }
 
     public static void test() {
-        var size = 20_000;
-        SolverOJA solver = new SolverOJA()
+        var size = 2000;
+        SolverJBLAS solver = new SolverJBLAS()
                 .setDimensions(size, size)
-                .setA(Primitive64Store.FACTORY.makeFilled(size, size, SparseRandomDistribution.of(0, 100, 0.8)))
-                .setB(Primitive64Store.FACTORY.makeFilled(size, 1, Uniform.of(0, 100)));
-        System.out.println(solver.a);
+                .setA(DoubleMatrix.rand(size, size))
+                .setB(DoubleMatrix.rand(size, 1));
         var start = System.currentTimeMillis();
         var success = true;
         try {
-            MatrixStore<Double> result = solver.solve();
-            var multiplicationResult = solver.a.multiply(result);
+            var result = solver.solve();
+            var multiplicationResult = solver.a.mmul(result);
             System.out.println("Diff between result and multiplication");
             double maxDiff = 0;
             for (int i = 0; i < size; i++) {
@@ -64,37 +65,36 @@ public class SolverOJA {
         }
     }
 
-    public SolverOJA setDimensions(int numberOfEquations, int numberOfVariables) {
+    public SolverJBLAS setDimensions(int numberOfEquations, int numberOfVariables) {
         this.numberOfEquations = numberOfEquations;
         this.numberOfVariables = numberOfVariables;
         return this;
     }
 
-    public SolverOJA setA(MatrixStore<Double> a) {
-        if (this.numberOfEquations == 0) this.numberOfEquations = a.getRowDim();
-        if (this.numberOfVariables == 0) this.numberOfVariables = a.getColDim();
-        if (this.numberOfEquations != a.getRowDim() || this.numberOfVariables != a.getColDim()) {
+    public SolverJBLAS setA(DoubleMatrix a) {
+        if (this.numberOfEquations == 0) this.numberOfEquations = a.rows;
+        if (this.numberOfVariables == 0) this.numberOfVariables = a.columns;
+        if (this.numberOfEquations != a.rows || this.numberOfVariables != a.columns) {
             throw new IllegalArgumentException("Matrix doesnt match required dimensions");
         }
         this.a = a;
         return this;
     }
 
-    public SolverOJA setB(MatrixStore<Double> b) {
-        if (this.numberOfEquations == 0) this.numberOfEquations = b.getRowDim();
-        if (this.numberOfEquations != b.getRowDim() || b.getColDim() != 1) {
+    public SolverJBLAS setB(DoubleMatrix b) {
+        if (this.numberOfEquations == 0) this.numberOfEquations = b.rows;
+        if (this.numberOfEquations != b.rows || b.columns != 1) {
             throw new IllegalArgumentException("Vector doesnt match required dimensions");
         }
         this.b = b;
         return this;
     }
 
-    public MatrixStore<Double> solve() throws SolvingFailedException {
-        final var solver = LU.R064.make(a);
+    public DoubleMatrix solve() throws SolvingFailedException {
         try {
-            return solver.solve(a, b);
-        } catch (RecoverableCondition e) {
-            throw new SolvingFailedException("Not solvable solution");
+            return Solve.solveSymmetric(a, b);
+        } catch (Throwable e) {
+            throw new SolvingFailedException(e);
         }
     }
 }
