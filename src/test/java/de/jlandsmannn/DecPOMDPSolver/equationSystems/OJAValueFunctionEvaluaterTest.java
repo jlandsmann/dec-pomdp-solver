8958151fc8feda8bdb84c2ddc8cc46a50080121d
jlandsmann <git@jlandsmann.de>
2024-05-06 08:02:34 +0200

    add unittests

---
+++ src/test/java/de/jlandsmannn/DecPOMDPSolver/equationSystems/OJAValueFunctionEvaluaterTest.java
@@ -0,0 +1,83 @@
+package de.jlandsmannn.DecPOMDPSolver.equationSystems;
+
+import de.jlandsmannn.DecPOMDPSolver.domain.finiteStateController.DecPOMDPWithStateController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.ojalgo.matrix.store.MatrixStore;
+import org.ojalgo.matrix.store.Primitive64Store;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OJAValueFunctionEvaluaterTest {
+
+  @Mock
+  private DecPOMDPWithStateController decPOMDP;
+
+  @Mock
+  private OJAValueFunctionTransformer valueFunctionTransformer;
+
+  @Mock
+  private OJAEquationSystemSolver solver;
+
+  @InjectMocks
+  private OJAValueFunctionEvaluater valueFunctionEvaluater;
+
+  private MatrixStore<Double> randomMatrix = Primitive64Store.FACTORY.make(3,3);
+
+  @BeforeEach
+  void setUp() {
+    when(solver.solve()).thenReturn(Optional.of(randomMatrix));
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallTransformerSetDecPOMDP() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(valueFunctionTransformer).setDecPOMDP(decPOMDP);
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallTransformerGetMatrix() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(valueFunctionTransformer).getMatrixFromDecPOMDP();
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallTransformerGetVector() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(valueFunctionTransformer).getVectorFromDecPOMDP();
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallEvaluatorSetMatrix() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(solver).setMatrix(null);
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallEvaluatorSetVector() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(solver).setVector(null);
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallEvaluatorSolve() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(solver).solve();
+  }
+
+  @Test
+  void evaluateValueFunction_ShouldCallTransformApplyValues() {
+    valueFunctionEvaluater.evaluateValueFunction(decPOMDP);
+    Mockito.verify(valueFunctionTransformer).applyValuesToDecPOMDP(randomMatrix);
+  }
+
+}
