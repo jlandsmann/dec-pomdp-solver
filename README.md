21ef098b35e3e6498c0cab7a049ec27d61cd1036
jlandsmann <git@jlandsmann.de>
2024-06-05 11:32:43 +0200

    add documentation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DecPOMDP Solver
 This repository contains a spring boot application for solving
 decentralized partial-observable markov decision problems.
-Solving means that the policies of the agents are optimised
+Solving means that the policies of the agents are optimized
 with respective of the expected reward starting from
 an initial belief state.
 
@@ -11,14 +11,63 @@ In our case, [openjdk-21](https://openjdk.org/projects/jdk/21/) was used.
 Additionally, you have to have [Maven 3.5+](https://maven.apache.org/download.cgi) installed.
 We used Maven 3.9.6.
 
-## How to use
+## How to use the shell
 You can run `mvn install` to generate a .jar-file from the source code you can execute.
 Otherwise, you can run `./mvnw spring-boot:run` to start the application directly.
 On Windows you can run `mvnw.cmd spring-boot:run` instead.
 
-## CLI
+### CLI
 This project contains a CLI, which will be started by default.
 There you have various commands to solve DecPOMDPs.
+
+### Scripts
+In the `scripts/` folder are some script files 
+that can be used within the Spring shell,
+by executing the command `script <file>` or `@<file>`.
+For each problem, there exists a script file for solving it.
+
+## How to use Docker
+We also provide a Docker image to run this application.
+This runs a script to solve the GridSmall problem 
+with the heuristic policy iteration solver by default.
+This behavior can be configured,
+either by setting the environment variable `PROBLEM`,
+or by setting the environment variable `SCRIPT`
+when running the image.
+
+To do so, you need to build the image by running
+```
+docker build -t dec-pomdp-solver .
+```
+and executing the image as a container by running
+```
+docker run -t dec-pomdp-solver 
+```
+
+## Testing
+There are many JUnit test suites to check the correctness of the implementation
+and to maintain the stability and quality of the implementation.
+Those will be automatically executed when calling maven scripts,
+like `mvn install`.
+If you want to execute those manually, you can run
+```
+mvn test
+```
+
+## Problem instance
+Currently, we have three different problem instances in our repository
+that can be solved by our heuristic policy iteration implementation.
+Here is a list of all problem instances (case-sensitive):
+
+1. DecTiger
+2. GridSmall
+3. BoxPushing
+
+All of them are originated to the
+[MADP Toolbox](https://www.fransoliehoek.net/fb/index.php?fuseaction=software.madp)
+by Frans Oliehoek and Matthijs Spaan.
+
+## Solver
 
 ### Heuristic policy iteration
 The heuristic policy iteration algorithm is based on the algorithm introduced in
--- src/main/java/de/jlandsmannn/DecPOMDPSolver/cmd/HeuristicPolicyIterationAlgorithmCommand.java
+++ src/main/java/de/jlandsmannn/DecPOMDPSolver/cmd/HeuristicPolicyIterationAlgorithmCommand.java
@@ -21,6 +21,11 @@ import java.util.stream.Collectors;
 
 import static org.springframework.shell.command.CommandRegistration.OptionArity;
 
+/**
+ * This command class contains all commands regarding the heuristic policy iteration algorithm.
+ * There are three main commands: init, load and solve.
+ * For more detailed information, have a look at the commands themselves.
+ */
 @Command(command = "heuristic", group = "Heuristic Policy Iteration", alias = "h")
 @Component
 public class HeuristicPolicyIterationAlgorithmCommand {
@@ -36,6 +41,9 @@ public class HeuristicPolicyIterationAlgorithmCommand {
     this.solver = solver;
   }
 
+  /**
+   * The help command prints some information about the used algorithm.
+   */
   @Command(command = "", alias = {"h"}, description = "Prints information about this algorithm.")
   public String help() {
     LOG.info("help command called.");
@@ -53,6 +61,12 @@ public class HeuristicPolicyIterationAlgorithmCommand {
       .toString();
   }
 
+  /**
+   * The init command initializes some configuration properties needed for the algorithm.
+   * This includes the number of belief points to generate,
+   * as well as the maximum number of iterations of the policy iteration to execute.
+   * After this command, the load command should be called.
+   */
   @Command(command = "init", alias = "i", description = "Initialize the heuristic policy iteration solver.")
   public String init(
     @Option(shortNames = 'k', defaultValue = "10") int numberOfBeliefPoints,
@@ -73,13 +87,22 @@ public class HeuristicPolicyIterationAlgorithmCommand {
       .toString();
   }
 
+  /**
+   * The load command needs to be executed, after the algorithm was initialized.
+   * The load command accepts a path to a file, as well as a (custom) discountFactor.
+   * The file will be loaded and parsed. It will be expected to follow the DPOMDP file format,
+   * which is described in the MADP Toolbox.
+   * The file must be located inside the resources folder of this application.
+   * If the file does not exist or is not a valid DPOMDP file, the parsing will be aborted.
+   * The (custom) discount factor can be used to override the discount factor defined in the loaded file.
+   */
   @Command(command = "load", alias = "l", description = "Load a problem instance to solve.")
   public String loadDecPOMDP(
     @Option(shortNames = 'f', required = true) String filename,
     @Option(shortNames = 'd', defaultValue = "-1") double discountFactor
   ) {
     LOG.info("Command 'load' was called with filename={}.", filename);
-    if (!initialized) return "Heuristic policy iteration is not initialized yet.";
+    if (!initialized) throw new IllegalStateException("Heuristic policy iteration is not initialized yet.");
     var optionalBuilder = DPOMDPFileParser.parseDecPOMDP(filename);
     if (optionalBuilder.isEmpty()) {
       LOG.warn("Parsing failed for file {}", filename);
@@ -101,13 +124,27 @@ public class HeuristicPolicyIterationAlgorithmCommand {
     return "Successfully loaded DecPOMDP";
   }
 
+  /**
+   * This command needs to be executed, after a DecPOMDP has been loaded.
+   * It can be used to set initial policies for the generation of belief points.
+   * Therefore, a space-separated list of probabilities for each action of each agent has to be defined,
+   * and those have to be enclosed by quotes.
+   * There must be such a list for each agent, separated by a space.
+   *
+   * `heuristic initialPolicy "0.8 0.1 0.1" "0.8 0.1 0.1"`
+   * Is an example for the DecTiger problem,
+   * where listening happens by a chance of 80% and hearing left or right each by a chance of 10%.
+   * Because both agents have same actions and policies, we repeat it.
+   */
   @Command(command = "initialPolicy", alias = "p", description = "Set initial policies for belief point generation.")
   public String setInitialPolicies(
     @Option(shortNames = 'a', required = true, arity = OptionArity.ONE_OR_MORE) String actionDistributions
   ) {
     LOG.info("Command 'initialPolicy' was called with actionDistributions={}.", actionDistributions);
-    var actionDistributionsPerAgent = actionDistributions.trim().split(",");
+    if (!initialized) throw new IllegalStateException("Heuristic policy iteration is not initialized yet.");
+    if (!loaded) throw new IllegalStateException("Heuristic policy iteration is not loaded yet.");
 
+    var actionDistributionsPerAgent = actionDistributions.trim().split(",");
     if (actionDistributionsPerAgent.length != decPOMDP.getAgents().size()) {
       throw new IllegalArgumentException("Number of given initial policies does not match the number of agents.");
     }
@@ -127,15 +164,22 @@ public class HeuristicPolicyIterationAlgorithmCommand {
     return "Successfully set initial policies";
   }
 
+  /**
+   * The solve command needs to be executed, after a DecPOMDP has been loaded.
+   * It starts the solving process and does not accept any arguments.
+   * It does not print information about interim steps,
+   * but prints the final result of the algorithm.
+   * For detailed information about the execution, have a look at the logs.
+   */
   @Command(command = "solve", alias = "s", description = "Solve the loaded problem instance.")
   public String solve() {
     LOG.info("Command 'solve' was called.");
     if (!initialized) {
       LOG.warn("Aborting solving because Heuristic policy iteration is not initialized yet.");
-      return "Heuristic policy iteration is not initialized yet.";
+      throw new IllegalStateException("Heuristic policy iteration is not initialized yet.");
     } else if (!loaded) {
       LOG.warn("Aborting solving because no DecPOMDP is loaded yet.");
-      return "Heuristic policy iteration is not loaded yet.";
+      throw new IllegalStateException("Heuristic policy iteration is not loaded yet.");
     }
     var result = solver.solve();
     LOG.info("Successfully solved DecPOMDP with value of {}", result);
