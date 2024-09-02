bef6e16f18fd8f3d84683779a2c47d8df3748b28
jlandsmann <git@jlandsmann.de>
2024-09-02 08:55:06 +0200

    improve unittests

--- src/test/java/de/jlandsmannn/DecPOMDPSolver/policyIteration/ExhaustiveBackupPerformerTest.java
+++ src/test/java/de/jlandsmannn/DecPOMDPSolver/policyIteration/ExhaustiveBackupPerformerTest.java
@@ -4,17 +4,17 @@ import de.jlandsmannn.DecPOMDPSolver.DecPOMDPGenerator;
 import de.jlandsmannn.DecPOMDPSolver.domain.decpomdp.IAgent;
 import de.jlandsmannn.DecPOMDPSolver.domain.decpomdp.primitives.State;
 import de.jlandsmannn.DecPOMDPSolver.domain.finiteStateController.DecPOMDPWithStateController;
+import de.jlandsmannn.DecPOMDPSolver.domain.finiteStateController.IAgentWithStateController;
+import de.jlandsmannn.DecPOMDPSolver.domain.finiteStateController.primitives.Node;
+import de.jlandsmannn.DecPOMDPSolver.domain.utility.CombinationCollectors;
 import de.jlandsmannn.DecPOMDPSolver.domain.utility.Distribution;
-import de.jlandsmannn.DecPOMDPSolver.domain.utility.VectorCombinationBuilder;
+import de.jlandsmannn.DecPOMDPSolver.domain.utility.Vector;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,21 +35,28 @@ class ExhaustiveBackupPerformerTest {
     var agentBeliefPoints = Set.of(
       Distribution.of(Map.of(
         decPOMDP.getStates().get(0), 0.5,
-        decPOMDP.getStates().get(decPOMDP.getStates().size() - 1), 0.5
+        decPOMDP.getStates().get(1), 0.5
       )),
       Distribution.of(Map.of(
         decPOMDP.getStates().get(0), 0.6,
-        decPOMDP.getStates().get(decPOMDP.getStates().size() - 1), 0.4
+        decPOMDP.getStates().get(1), 0.4
       )),
       Distribution.of(Map.of(
         decPOMDP.getStates().get(0), 0.4,
-        decPOMDP.getStates().get(decPOMDP.getStates().size() - 1), 0.6
+        decPOMDP.getStates().get(1), 0.6
       ))
     );
     beliefPoints = Map.of(
       decPOMDP.getAgents().get(0), agentBeliefPoints,
-      decPOMDP.getAgents().get(decPOMDP.getStates().size() - 1), agentBeliefPoints
+      decPOMDP.getAgents().get(1), agentBeliefPoints
     );
+
+    var random = new Random();
+    decPOMDP.getStates().stream().parallel().forEach(state -> {
+      decPOMDP.getNodeCombinations().stream().parallel().forEach(nodeVector -> {
+        decPOMDP.setValue(state, nodeVector, random.nextDouble());
+      });
+    });
   }
 
   @Test
@@ -124,21 +131,40 @@ class ExhaustiveBackupPerformerTest {
   }
 
   @Test
+  void performExhaustiveBackupForAgent_ShouldOnlyAddNodesWithDeterministicActionSelection() {
+    var agent = decPOMDP.getAgents().get(0);
+    var addedNodes = performExhaustiveBackupForAgentAndReturnAddedNodes(agent);
+
+    for (var addedNode : addedNodes) {
+      var expectedNumberOfActions = 1;
+      var actualNumberOfActions = agent.getSelectableActions(addedNode).size();
+      assertEquals(expectedNumberOfActions, actualNumberOfActions);
+    }
+
+  }
+
+  @Test
+  void performExhaustiveBackupForAgent_ShouldOnlyAddNodesWithDeterministicObservationTransition() {
+    var agent = decPOMDP.getAgents().get(0);
+    var addedNodes = performExhaustiveBackupForAgentAndReturnAddedNodes(agent);
+
+    for (var addedNode : addedNodes) {
+      for (var action : agent.getSelectableActions(addedNode)) {
+        for (var observation : agent.getObservations()) {
+          var expectedNumberOfFollowNodes = 1;
+          var actualNumberOfFollowNodes = agent.getFollowNodes(addedNode).stream().filter(followNode -> agent.getNodeTransitionProbability(addedNode, action, observation, followNode) > 0).count();
+
+          assertEquals(expectedNumberOfFollowNodes, actualNumberOfFollowNodes);
+        }
+      }
+    }
+  }
+
+  @Test
   void updateValueFunction_ShouldUpdateAllMissingValuesOfValueFunctionForBeliefPoints() {
     var agent = decPOMDP.getAgents().get(0);
-    var originalNodes = List.copyOf(agent.getControllerNodes());
-    exhaustiveBackupPerformer
-      .setDecPOMDP(decPOMDP)
-      .setBeliefPoints(beliefPoints)
-      .performExhaustiveBackupForAgent(agent);
-    var addedNodes = agent.getControllerNodes();
-    addedNodes.removeAll(originalNodes);
-
-    var rawNodeCombinations = decPOMDP.getAgents().stream().map(a -> {
-      if (a.equals(agent)) return addedNodes;
-      else return a.getControllerNodes();
-    }).toList();
-    var newNodeCombinations = VectorCombinationBuilder.listOf(rawNodeCombinations);
+    var addedNodes = performExhaustiveBackupForAgentAndReturnAddedNodes(agent);
+    var newNodeCombinations = getNodeCombinationsForAgent(agent, addedNodes);
 
     for (var state : decPOMDP.getStates()) {
       for (var nodeCombination : newNodeCombinations) {
@@ -148,16 +174,42 @@ class ExhaustiveBackupPerformerTest {
 
     exhaustiveBackupPerformer.updateValueFunction();
 
-    var statesInBeliefPoints = beliefPoints.values().stream()
-      .flatMap(Collection::stream)
-      .map(Distribution::keySet)
-      .flatMap(Set::stream)
-      .collect(Collectors.toSet());
-    for (var state : statesInBeliefPoints) {
+
+    for (var state : getBeliefPointStates()) {
       for (var nodeCombination : newNodeCombinations) {
         var optionalValue = decPOMDP.hasValue(state, nodeCombination);
         assertTrue(optionalValue, "Missing value for " + state + " and " + nodeCombination);
       }
     }
+  }
+
+  private List<Node> performExhaustiveBackupForAgentAndReturnAddedNodes(IAgentWithStateController agent) {
+    var originalNodes = List.copyOf(agent.getControllerNodes());
+    exhaustiveBackupPerformer
+      .setDecPOMDP(decPOMDP)
+      .setBeliefPoints(beliefPoints)
+      .performExhaustiveBackupForAgent(agent);
+    var addedNodes = agent.getControllerNodes();
+    addedNodes.removeAll(originalNodes);
+
+    return addedNodes;
+  }
+
+  private List<Vector<Node>> getNodeCombinationsForAgent(IAgentWithStateController agent, List<Node> addedNodes) {
+    return decPOMDP.getAgents().stream()
+      .map(a -> {
+        if (a.equals(agent)) return addedNodes;
+        else return a.getControllerNodes();
+      })
+      .collect(CombinationCollectors.toCombinationVectors())
+      .toList();
+  }
+
+  private Set<State> getBeliefPointStates() {
+    return beliefPoints.values().stream()
+      .flatMap(Collection::stream)
+      .map(Distribution::keySet)
+      .flatMap(Set::stream)
+      .collect(Collectors.toSet());
   }
 }
